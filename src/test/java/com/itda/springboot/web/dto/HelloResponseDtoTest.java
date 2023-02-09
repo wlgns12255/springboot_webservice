1eb90d87f65a1aa70b058ea1903fae30a1b588f3
Parkjihun <qkrmfqkrmf97@gmail.com>
2023-02-09 23:43:55 +0900

    HelloController 초기 버전 -SpringBoot 실행 -JUnit을 통한 단위테스트 -lombok을 사용해 매퍼클래스 생성

---
+++ src/test/java/com/itda/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.itda.springboot.web.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.itda.springboot.web.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬북_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
